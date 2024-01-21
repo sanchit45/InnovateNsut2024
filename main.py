794d2df13cdfa3b8f6c32376f075d7042f629de4
Sanchit Baweja <99529694+sanchit45@users.noreply.github.com>
2024-01-21 08:00:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ async def assistive_legalaid(data: InputData):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
